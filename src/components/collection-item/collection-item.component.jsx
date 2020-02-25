8e8f95be0090bcee1f1882a0c698b7f19b617a40
Dylan <dcren2010@gmail.com>
2020-02-25 14:41:36 -0500

    adding header component and links to home, shop page and contact

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,7 +4,7 @@ import './collection-item.styles.scss'
 const CollectionItem = ({name, price, imageUrl}) => (
     <div className='collection-item'>
         <div className='image'
-             style={{backgroundImage: `url${imageUrl}`}}
+             style={{backgroundImage: `url(${imageUrl})`}}
         />
         <div className='collection-footer'>
             <span className='name'>{name}</span>
